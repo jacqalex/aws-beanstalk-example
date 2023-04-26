d7806c3cdd496be1e9e4a03d44b1d2c8a05fb341
Jacqueline Mkindi <jacqalex2002@gmail.com>
2023-04-26 15:07:42 -0500

    AWS BeanStalk example - inital commit

---
+++ src/test/java/com/myproject/aws/AwsElasticBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
